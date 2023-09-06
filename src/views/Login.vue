2213fed3e8f730312e5e217bd4903b340854140d
Stonerunner24 <eli.coyle1@gmail.com>
2023-09-06 09:39:49 -0500

    Deleted Lesson pages, and tutorial view, kept add, edit, and list tutorial.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,8 +7,5 @@ import SocialLogin from "../components/SocialLogin.vue";
     <v-toolbar>
       <v-toolbar-title>Welcome to Tutorials!</v-toolbar-title>
     </v-toolbar>
-    <br /><br />
-    <SocialLogin />
-    <br /><br />
   </v-container>
 </template>
