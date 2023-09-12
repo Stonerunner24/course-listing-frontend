51542e65dce75756c2b499ea73fd529795f1c760
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2023-09-12 16:54:32 -0500

    made basic v-container

--- src/components/CourseView.vue
+++ src/components/CourseView.vue
@@ -1,10 +1,37 @@
 <script setup>
-    
+    import {ref, onMounted} from 'vue'
+    import courseServices from '../services/courseServices';
+
+    const course = ({
+        department: "",
+        number: "",
+        level: "",
+        hours: "",
+        name: "",
+        description:""
+    })
+
+    const id = ref(null)
+
+    onMounted(() => {
+        courseServices.get(id.value)
+            .then((response) => {
+                 course.value = response.data
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    })
 </script>
 
 <template>
     <div>
-
+        <v-container>
+            <v-card elevation="5">
+                <v-card-title>course number{{ course.number }} course name{{ course.name }}</v-card-title>
+                <v-card-text>description{{ course.description }}</v-card-text>
+            </v-card>
+        </v-container>
     </div>
 </template>
 
