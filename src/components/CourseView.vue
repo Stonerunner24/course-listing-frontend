7b4d5d82acc0fd9874afff0929026d69bad5031f
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2023-09-13 09:39:34 -0500

    finished skeleton of view component

--- src/components/CourseView.vue
+++ src/components/CourseView.vue
@@ -22,16 +22,22 @@
                 console.log(error)
             })
     })
+
 </script>
 
 <template>
     <div>
         <v-container>
-            <v-card elevation="5">
-                <v-card-title>course number{{ course.number }} course name{{ course.name }}</v-card-title>
-                <v-card-text>description{{ course.description }}</v-card-text>
-            </v-card>
+            <b-overlay :show="show" rounded="sm">
+                <v-card elevation="5" rounded="lg" class="pa-10">
+                    <v-card-title>course number{{ course.number }} course name{{ course.name }}</v-card-title>
+                    <v-card-text class="pa-4">Hours: {{ hours }}</v-card-text>
+                    <v-card-text class="pa-4">description{{ course.description }}</v-card-text>
+                    <v-btn rounded="lg" elevation="2" class="float-right" color="#D5DFE7">
+                        <div class="v-button_content">Close</div>
+                    </v-btn>
+                </v-card>
+            </b-overlay>
         </v-container>
     </div>
 </template>
-
