e84762f0ec329387baa994722c55398ee948fabf
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2023-09-13 16:25:29 -0500

    finishing touches until list is done

--- src/components/CourseView.vue
+++ src/components/CourseView.vue
@@ -5,35 +5,9 @@
 
     export default {
     props: {
-        department: String,
-        number: String,
-        hours: Number,
-        level: Number,
-        name: String,
-        description: String
+        course: Object,
     },
     }
-
-    // const course = defineProps({
-    //     department: String,
-    //     number: String,
-    //     hours: Number,
-    //     level: Number,
-    //     name: String,
-    //     description: String,
-    // })
-
-
-
-    // onMounted(() => {
-    //     courseServices.get(id.value)
-    //         .then((response) => {
-    //              course.value = response.data
-    //         })
-    //         .catch((error) => {
-    //             console.log(error)
-    //         })
-    // })
 
 </script>
 
@@ -42,9 +16,9 @@
         <v-container>
             <b-overlay :show="show" rounded="sm">
                 <v-card elevation="5" rounded="lg" class="pa-10">
-                    <v-card-title>course number{{ course.number }} course name{{ course.name }}</v-card-title>
-                    <v-card-text class="pa-4">Hours: {{ hours }}</v-card-text>
-                    <v-card-text class="pa-4">description{{ course.description }}</v-card-text>
+                    <v-card-title>course number course name</v-card-title>
+                    <v-card-text class="pa-4">Hours: </v-card-text>
+                    <v-card-text class="pa-4">description</v-card-text>
                     <v-btn @click="onClick" rounded="lg" elevation="2" class="float-right" color="#D5DFE7">
                         <div class="v-button_content">Close</div>
                     </v-btn>
