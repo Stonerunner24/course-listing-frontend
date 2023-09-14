ef749979286768071589f471a25ed39a870afaae
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-09-14 00:22:52 -0500

    Added dialog functionality, emits and props

--- src/components/DeleteCourse.vue
+++ src/components/DeleteCourse.vue
@@ -1,20 +1,18 @@
 <template>
     <div>
         <v-container>
-            <b-overlay :show="show" rounded="sm">
-                <v-card elevation="5" rounded="lg" class="pa-10">
-                    <v-card-title>Delete {{ id }}?</v-card-title>
-                    <v-card-text>This action is permanent. Are you sure you want to delete {{ id }}?</v-card-text>
-                    <v-card-actions>
-                        <v-btn color="red" raised>
-                            Delete
-                        </v-btn>
-                        <v-btn flat>
-                            Cancel
-                        </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </b-overlay>
+            <v-card elevation="5" rounded="lg" class="pa-10">
+                <v-card-title>Delete {{ id }}?</v-card-title>
+                <v-card-text>This action is permanent. Are you sure you want to delete {{ id }}?</v-card-text>
+                <v-card-actions>
+                    <v-btn color="red" elevation="5" @click="$emit('delete', id)">
+                        Delete
+                    </v-btn>
+                    <v-btn flat @click="$emit('cancel')">
+                        Cancel
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
         </v-container>
     </div>
 </template>
@@ -25,6 +23,7 @@ import { ref, onMounted } from "vue";
 export default {
     props: {
         id: String // The course number of the course to be deleted, passed in from parent
-    }
+    },
+    emits: ['delete', 'cancel']
 }
 </script>
--- src/views/CourseList.vue
+++ src/views/CourseList.vue
@@ -3,11 +3,14 @@ import CourseServices from "../services/courseServices";
 import Utils from "../config/utils.js";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import DeleteCourse from "../components/DeleteCourse.vue"
 
 
 const router = useRouter();
 const courses = ref([]);
 const message = ref("View, edit, or delete courses.");
+const deleteModal = ref(false);
+const selectedCourse = ref(null);
 const headers = [
   {
     text: 'Course Number',
@@ -29,9 +32,16 @@ const viewCourse = (course) => {
   router.push({name: "view", params: {course}})
 }
 
-const deleteCourse = (course) => {
-  console.log(course.course_number);
-  CourseServices.delete(course.course_number)
+const handleDelete = (course_number) => {
+  console.log(course_number);
+  deleteModal.value = false; 
+  selectedCourse.value = null;
+  deleteCourse(course_number);
+}
+
+const deleteCourse = (course_number) => {
+  console.log(course_number);
+  CourseServices.delete(course_number)
     .then(() => {
       retrieveCourses();
     })
@@ -54,19 +64,6 @@ const retrieveCourses = () => {
 
 retrieveCourses();
 
-// const data = () => {
-//   return{
-//     headers: [
-//       {
-//         text: 'Course Number',
-//         align: 'left',
-//         sortable: false,
-//         value: 'course_number',
-//       },
-//       {text: 'Name', value: 'course_name'}
-//     ]
-//   };
-// };
 
 
 </script>
@@ -91,7 +88,7 @@ retrieveCourses();
         </v-card-text>
         <v-table>
           <tbody>
-            <tr v-for="(item, index) in courses" :key="item.title" style="border: 5px solid transparent">
+            <tr v-for="(item, index) in courses" :key="item.course_number" style="border: 5px solid transparent">
               <td>{{ item.course_name }}</td>
               <td>{{ item.course_number }}</td>
               <td>
@@ -101,7 +98,7 @@ retrieveCourses();
                 <v-icon small class="mx-4" @click="viewCourse(item)">
                   mdi-format-list-bulleted-type
                 </v-icon>
-                <v-icon small class="mx-4" @click="deleteCourse(item)">
+                <v-icon small class="mx-4" @click.stop="(deleteModal = true), (selectedCourse = item.course_number)">
                   mdi-trash-can
                 </v-icon>
               </td>
@@ -110,5 +107,16 @@ retrieveCourses();
         </v-table>
       </v-card>
     </v-container>
+    <v-dialog
+        v-model="deleteModal"
+        activator="parent"
+        width="auto"
+      >
+      <DeleteCourse 
+        :id="selectedCourse"
+        @delete="handleDelete"
+        @cancel="(deleteModal = false), (selectedCourse = null)"/>
+      
+    </v-dialog>
   </div>
 </template>
